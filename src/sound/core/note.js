035e2cadfcf4cf9c7abf68dd0c8f9660669d41b8
Antoine Cordier <com.acordier@gmail.com>
2017-09-10 20:54:08 +0200

    Add doc comments

--- src/sound/core/note.js
+++ src/sound/core/note.js
@@ -17,14 +17,17 @@ export const Note = ({ note, octave, duration }) => ({
 	},
 })
 
+/**
+ * Computes the frequency value of the given note in the given octave
+ * @param {string} note - Note in scale (english notation)
+ * @param {number} octave - Octave value for note
+ */
 export const getFrequency = (note, octave) => {
-	// ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
-	// ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
 	const notes = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
-	const base = 4
+	const base = 4 // base octave
 	const n = notes.indexOf(note)
-	const f0 = 440
+	const f0 = 440 // base frequency
 	const a = 2 ** (1 / 12)
-	const m = octave - base >= 0 ? ((octave - base) + 1) : 1 / ((base - octave) + 1)
+	const m = octave - base >= 0 ? ((octave - base) + 1) : 1 / ((base - octave) + 1) // multiplier
 	return f0 * (a ** n) * m
 }
