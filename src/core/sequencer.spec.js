6c7a0539c56c93049f69a77157ad2d934d197964
a-cordier <com.acordier@gmail.com>
2017-09-13 15:27:21 +0200

    Fix typo in test description

--- src/core/sequencer.spec.js
+++ src/core/sequencer.spec.js
@@ -3,7 +3,7 @@ import sinon from 'sinon'
 import { Sequencer } from '.'
 import { AudioContextMock } from '../mock/audio-context.mock'
 
-test('Calling start trigger onPlay handler', (t) => {
+test('Calling start triggers onPlay handler', (t) => {
 	const audioContext = AudioContextMock(sinon.sandbox.create())
 	Sequencer({ audioContext })
 		.onPlay(() => {
