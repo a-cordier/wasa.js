b5cdb91573e23c4905d2cd427bcfba39022a5296
a-cordier <com.acordier@gmail.com>
2017-09-23 20:22:01 +0200

    Make knobs on their way

---
+++ src/components/knob.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <svg class="knob" viewBox="0 0 40 40">
+      <circle class="mainCircle" cx="20" cy="20" r="15" />
+      <line x1="20" y1="20" x2="20" y2="10" />
+    </svg>
+  </div>
+</template>
+
+<style scoped>
+.knob {
+  width: 2vw;
+  height: 2vw;
+  user-select: none;
+}
+
+circle.mainCircle {
+  fill: #ccc;
+  stroke: #bbb;
+  stroke-width: 5px;
+}
+
+circle.mainCircle.active {
+  fill: #8ab9b5;
+}
+
+line {
+  stroke: #444;
+  stroke-width: 5px;
+  stroke-linecap: round;
+}
+</style>
+
