851560e653c2920ecf3e42aa361b7ea515612c41
a-cordier <com.acordier@gmail.com>
2017-09-23 02:44:32 +0200

    Add drum sequencer component

---
+++ src/components/step-tracker.vue
@@ -0,0 +1,54 @@
+<style lang="sass" scoped>
+#tracker {
+    margin-top: 10px;
+    display: grid;
+    grid-template-columns: repeat(16, 1fr);
+    .step {
+        justify-self: center;
+        width: 2vw;
+        height: 1vh;
+        &.inactive {
+            background-color: #995574;
+        }
+        &.active {
+            background-color: #ff5574;
+        }
+    }
+}
+</style>
+
+<template>
+    <div id="tracker">
+        <div v-for="step in steps" :key="step.id" 
+            class="step" v-bind:class="[step.state]"></div>
+    </div>
+</template>
+
+<script>
+import { times } from 'ramda'
+import { Dispatcher, Events } from '../wasa/common/dispatcher'
+
+export default {
+    created() {
+        Dispatcher.as(Events.SEQUENCER_TICK)
+            .subscribe((tick) => {
+                const step = this.steps.find(step => step.state === 'active')
+                if(step) {
+                    step.state = 'inactive'
+                }
+                this.steps[tick-1].state = 'active'
+            })
+        Dispatcher.as(Events.SEQUENCER_STOP)
+            .subscribe(() => {
+                this.steps.forEach((step) => {
+                    step.state = 'inactive'
+                })
+            })
+    },
+    methods: {},
+    data: () => ({
+        steps: times((n) => ({ state: 'inactive' }), 16)
+    })
+}
+</script>
+
