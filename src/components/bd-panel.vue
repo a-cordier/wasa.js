8e1f03cc708160daa12330ce3b7ea6cbd93b7b87
Antoine <com.acordier@gmail.com>
2017-12-18 23:33:50 +0100

    Refactor(panels) Add toggle button

--- src/components/bd-panel.vue
+++ src/components/bd-panel.vue
@@ -1,72 +1,72 @@
-<style lang="scss"  type="text/scss">
- #bd-panel {
-    .parameters {
-      .knob {
-        display: block;
-        width: 25px;
-		    margin: 10px auto 0 auto;
-	    }
-    }
- }
+<style lang="scss" type="text/scss">
+	#bd-panel {
+		.parameters {
+			/deep/ .toggle {
+				margin: 10px auto 0 auto;
+			}
+			.knob {
+				display: block;
+				width: 60px;
+				margin: 10px auto 0 auto;
+			}
+		}
+	}
 
 
 </style>
 
 <template>
-    <div id="bd-panel">
-        <div class="parameters">
-            <knob class="knob" :value="getFrequencyValue()" @update="setFrequencyValue" :label="'freq.'"></knob>
-            <knob class="knob" :value="getFinalFrequencyValue()" @update="setFinalFrequencyValue" :label="'final freq.'"></knob>
-            <knob class="knob" :value="getDurationValue()" @update="setDurationValue" :label="'duration'"></knob>
+	<div id="bd-panel">
+		<div class="parameters">
+			<knob class="knob" :value="getFrequencyValue()" @update="setFrequencyValue" :label="'freq.'"></knob>
+			<knob class="knob" :value="getDurationValue()" @update="setDurationValue" :label="'duration'"></knob>
 			<knob class="knob" :value="getOutputGainValue()" @update="setOutputGainValue" :label="'gain'"></knob>
-        </div>
-    </div>
+			<toggle @toggle="mute" :label="'mute'"></toggle>
+		</div>
+	</div>
 </template>
 
 <script>
-import Knob from "./knob.vue"
-import { unscale, scale } from "wasa"
+	import Knob from './knob.vue'
+	import Toggle from './toggle.vue'
+	import { unscale, scale } from 'wasa'
+	import { ifElse, identity } from 'ramda'
 
-export default {
-  props: {
-    drum: { type: Object }
-  },
-  components: {
-    Knob
-  },
-  created() {},
-  data: () => ({}),
-  methods: {
-    setFrequencyValue(value) {
-      this.drum.setFrequencyValue(unscale({ min: 100, max: 200 }, value))
-    },
-    getFrequencyValue() {
-      return scale({ min: 100, max: 200 }, this.drum.getFrequencyValue())
-    },
-    setFinalFrequencyValue(value) {
-      this.drum.setFinalFrequencyValue(unscale({ min: 1E-10, max: 10 }, value))
-    },
-    getFinalFrequencyValue() {
-      return scale({ min: 1E-10, max: 10 }, this.drum.getFinalFrequencyValue())
-    },
-    setDurationValue(value) {
-      this.drum.setDurationValue(unscale({ min: 0.5, max: 2 }, value))
-	},
-	getDurationValue() {
-      return scale({ min: 0.5, max: 2 }, this.drum.getDurationValue())
-    },
-    getAttackValue() {
-      return scale({ min: 1E-5, max: 0.25 }, this.drum.getAttackValue())
-	},
-	setAttackValue(value) {
-      this.drum.setAttackValue(unscale({ min: 1E-5, max: 0.25 }, value))
-    },
-	setOutputGainValue(value) {
-      this.drum.setOutputGainValue(value)
-    },
-    getOutputGainValue() {
-      return this.drum.getOutputGainValue()
-    }
-  }
-}
+	export default {
+		props: {
+			drum: { type: Object }
+		},
+		components: {
+			Knob,
+			Toggle,
+		},
+		created() {
+		},
+		data: () => ({
+
+		}),
+		methods: {
+			setFrequencyValue(value) {
+				this.drum.setFrequencyValue(unscale({ min: 140, max: 160 }, value))
+			},
+			getFrequencyValue() {
+				return scale({ min: 140, max: 160 }, this.drum.getFrequencyValue())
+			},
+			setDurationValue(value) {
+				this.drum.setDurationValue(unscale({ min: 0.15, max: 0.5 }, value))
+			},
+			getDurationValue() {
+				return scale({ min: 0.15, max: 0.5 }, this.drum.getDurationValue())
+			},
+			setOutputGainValue(value) {
+				this.drum.setOutputGainValue(value)
+			},
+			getOutputGainValue() {
+				return this.drum.getOutputGainValue()
+			},
+			mute(active) {
+				ifElse(identity, this.drum.mute, this.drum.unMute)(active)
+			},
+		}
+	}
 </script>
---
+++ src/components/toggle.vue
@@ -0,0 +1,58 @@
+<style lang="scss" type="text/scss" scoped>
+	.toggle {
+		button {
+			width: 15px;
+			height: 15px;
+			border-radius: 50%;
+			outline: none;
+			display: block;
+			margin: 10px auto;
+			&.inactive {
+				background-color: #995574;
+			}
+			&.active {
+				background-color: #ff5574;
+				box-shadow: 0px 0px 5px 0px #ff5574;
+
+			}
+		}
+		.label {
+			font-size: 9px;
+			text-align: center;
+			display: block;
+			margin: 0 auto;
+			color: white;
+			font-family: "Lucida Console", Monaco, monospace;
+		}
+	}
+</style>
+
+<template>
+	<div class="toggle">
+		<button :class="{ active, inactive: !active }" @click="toggle()"></button>
+		<span class="label">{{label}}</span>
+	</div>
+</template>
+
+<script>
+	import { times } from 'ramda'
+	import { Dispatcher, Events } from 'wasa'
+
+	export default {
+		props: {
+			label: {
+				type: String,
+			}
+		},
+		methods: {
+			toggle() {
+				this.active = !this.active
+				this.$emit('toggle', this.active)
+			},
+		},
+		data: () => ({
+			active: false
+		})
+	}
+</script>
+
